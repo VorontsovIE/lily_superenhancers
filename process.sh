2df3ab127f239d13a6a765e6ef104d937870aadb
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2018-10-22 17:14:07 +0300

    normalize enhancer files

--- process.sh
+++ process.sh
@@ -2,3 +2,13 @@
 ruby cistrome_merger_cmd.rb | bash
 # Store number of peaks in each cistrome
 ruby cistrome_num_peaks.rb > results/cistrome_num_peaks.tsv
+
+# Normalize (sort) enhancers in each file
+find source_data/enhancers_bed/ -xtype f | xargs -n1 -I{} echo 'bedtools sort -i {} | sponge {}' | bash
+
+# Split enhancers into separate files: superenhancers/enhancers/promoters
+for i in `seq 1 6`; do
+  awk -e '($4=="SE"){print $0}' -- source_data/enhancers_bed/${i}_h3k27ac.scores.bed > results/genomic_intervals/superenhancers/superenhancers_${i}.bed
+  awk -e '($4=="promoter"){print $0}' -- source_data/enhancers_bed/${i}_h3k27ac.scores.bed > results/genomic_intervals/promoters/promoters_${i}.bed
+  awk -e '($4=="enhancer"){print $0}' -- source_data/enhancers_bed/${i}_h3k27ac.scores.bed > results/genomic_intervals/enhancers/enhancers_${i}.bed
+done
